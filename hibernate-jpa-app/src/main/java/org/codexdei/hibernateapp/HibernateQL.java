0ebb583e54a73dda459cb1741ce269c2c37710c8
codexdei <jlacostc@gmail.com>
2025-08-26 20:52:45 -0500

    se agregan mas consultas JPQL

--- hibernate-jpa-app/src/main/java/org/codexdei/hibernateapp/HibernateQL.java
+++ hibernate-jpa-app/src/main/java/org/codexdei/hibernateapp/HibernateQL.java
@@ -4,6 +4,9 @@ import jakarta.persistence.EntityManager;
 import org.codexdei.hibernateapp.domain.CustomerDTO;
 import org.codexdei.hibernateapp.entity.Customer;
 import org.codexdei.hibernateapp.util.JpaUtil;
+import org.hibernate.hql.internal.ast.tree.IdentNode;
+import org.hibernate.tool.schema.internal.exec.ScriptTargetOutputToStdout;
+import org.w3c.dom.ls.LSOutput;
 
 import java.util.List;
 import java.util.Objects;
@@ -75,6 +78,142 @@ public class HibernateQL {
                 .getResultList();
         customerDTOS.forEach(System.out::println);
 
+        System.out.println("============== Query with the names of customers ================");
+        List<String> names = em.createQuery("select c.name from Customer c", String.class)
+                .getResultList();
+        names.forEach(System.out::println   );
+
+        System.out.println("============== Query with distinct customers names ================");
+        names = em.createQuery("select distinct(c.name) from Customer c", String.class)
+                .getResultList();
+        names.forEach(System.out::println);
+
+        System.out.println("============== Query with distinct payment methods ================");
+        List<String> paymentsDistinct = em.createQuery("select distinct(c.paymentMethod) from Customer c", String.class)
+                .getResultList();
+        paymentsDistinct.forEach(System.out::println);
+
+        System.out.println("============== Query to count the payment method  ================");
+        Long paymmentMethodNumber = em.createQuery("select count(distinct c.paymentMethod) from Customer c",Long.class)
+                .getSingleResult();
+        System.out.println("Number payment method: " + paymmentMethodNumber);
+
+        //Muestra nombre y apellido concatendados
+        System.out.println("================== Query with name and lastName concatenated ==============");
+        names = em.createQuery("select concat(c.name, ' ', c.lastName) as namelastname from Customer c", String.class)
+                .getResultList();
+        names.forEach(System.out::println);
+
+        //Muestra nombre y apellido concatendados
+        System.out.println("================== Query with name and lastName concatenated ==============");
+        names = em.createQuery("select c.name || ' ' || c.lastName as namelastname from Customer c", String.class)
+                .getResultList();
+        names.forEach(System.out::println);
+
+        System.out.println("================== Query with name and lastName upper concatenated ==============");
+        names = em.createQuery("select upper(concat(c.name,' ',c.lastName)) as namelastname from Customer c",String.class)
+                .getResultList();
+        names.forEach(System.out::println);
+
+        System.out.println("================== Query with name and lastName lower concatenated ==============");
+        names = em.createQuery("select lower(concat(c.name,' ',c.lastName)) as namelastname from Customer c",String.class)
+                .getResultList();
+        names.forEach(System.out::println);
+
+        System.out.println("================== Query to search by name ==============");
+        String param = "ui";
+        //busca coincideas de nombres segun el "param"(variable de la linea anterior),es decir nombres que contengan esas letras
+        //mysql no es sensible a mayusculas o minusculas por lo que se podria escribir en mayusculas o minusculas
+        //se coloca "upper" para convertir a mayusculas, ES UNA BUENA PRACTICA PARA CASOS DONDE SE USO OTRO MOTOR SQL que
+        //no sea mysql y que si sea sensible a mayusculas o minusculas
+        customers = em.createQuery("select c from Customer c where upper(c.name) like upper(:parameter)",Customer.class)
+                //los porcentajes("%")se colocan para que busque coincidencias en todo el nombre, sino se colocan
+                //no hace la busqueda
+                .setParameter("parameter", "%" + param + "%")
+                .getResultList();
+        customers.forEach(System.out::println);
+
+        System.out.println("================== Query between numbers ==============");
+        //consultas entre(between) rangos numericos
+        customers = em.createQuery("select c from Customer c where c.id between 5 and 10", Customer.class)
+                .getResultList();
+        customers.forEach(System.out::println);
+
+        System.out.println("================== Query between letters ==============");
+        customers = em.createQuery("select c from Customer c where c.name between 'L' and 'Z'", Customer.class)
+                .getResultList();
+        customers.forEach(System.out::println);
+
+        System.out.println("================== Query Order by ==============");
+        //oder por algun elemento, en el siguiente ejemplo por nombres, asc=ascendente des=descendente
+        customers = em.createQuery("select c from Customer c order by c.name asc", Customer.class)
+                .getResultList();
+        customers.forEach(System.out::println);
+        //Tambien se puede ordenar por un elemento, luego por otro, por ejemplo por apellido y si hay
+        //apellidos repetidos ordena por nombre
+        customers = em.createQuery("select c from Customer c order by c.lastName asc, c.name desc", Customer.class)
+                .getResultList();
+        customers.forEach(System.out::println);
+
+        System.out.println("================== Query count ==============");
+        //contar elementos
+        Long total = em.createQuery("select count(c) as total from Customer c", Long.class)
+                .getSingleResult();
+        System.out.println("Customers Total:" + total);
+
+        System.out.println("================== Query min ==============");
+        Long minId = em.createQuery("select min(c.id) as min from Customer c", Long.class).getSingleResult();
+        System.out.println("Min ID: " + minId);
+
+        System.out.println("================== Query max ==============");
+        Long maxId = em.createQuery("select max(c.id) as max from Customer c", Long.class).getSingleResult();
+        System.out.println("max ID:" + maxId);
+
+        System.out.println("================== Query the name and its length ==============");
+        registries = em.createQuery("select c.name, length(c.name) from Customer c", Object[].class)
+                .getResultList();
+        registries.forEach(reg -> {
+            String nam = (String) reg[0];
+            Integer length = (Integer) reg[1];
+            System.out.println("Name = " + nam + ", lenght = " + length);
+        });
+
+        System.out.println("================== Query the shortest name ==============");
+        Integer nameShortest = em.createQuery("select min(length(c.name)) from Customer c", Integer.class).getSingleResult();
+        System.out.println("Shortest Name: " + nameShortest);
+
+        System.out.println("================== Query the longest name ==============");
+        Integer nameLongest = em.createQuery("select max(length(c.name)) from Customer c", Integer.class).getSingleResult();
+        System.out.println("Longest Name: " + nameLongest);
+
+        System.out.println("================== Query statistics ==============");
+        Object[] statistics = em.createQuery("select min(c.id), max(c.id), count(c.id), sum(c.id), avg(length(c.lastName)) from Customer c",
+                        Object[].class)
+                .getSingleResult();
+        Long minSta = (Long) statistics[0];
+        Long maxSta = (Long) statistics[1];
+        Long count = (Long) statistics[2];
+        Long sum = (Long) statistics[3];
+        Double avg = (Double) statistics[4];
+        System.out.println("Min id = " + minSta);
+        System.out.println("Max id = " + maxSta);
+        System.out.println("Count id = " + count);
+        System.out.println("Sum id = " + sum);
+        System.out.println("Average lastName = " + avg);
+
+        System.out.println("================== Query the shortest name and its length ==============");
+        registries = em.createQuery("select c.name, length(c.name) from Customer c where " +
+                                        "length(c.name) = (select min(length(c.name)) from Customer c)", Object[].class)
+                .getResultList();
+        registries.forEach(reg -> {
+            String nam = (String) reg[0];
+            Integer length = (Integer) reg[1];
+            System.out.println("Shortest name:" + nam + ", length: " + length);
+        });
+
+
+
+        //Cerramos conexion
         em.close();
     }
 }
